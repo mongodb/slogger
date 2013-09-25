7433b1114f19b89502fdfd2dcba3aa832633ea78
Tim Olsen <tim.olsen@mongodb.com>
2013-09-25 12:57:39 -0400

    Add AsyncAppender

---
+++ v2/async_appender/async_appender_test.go
@@ -0,0 +1,152 @@
+// Copyright 2013 MongoDB, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package async_appender
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"runtime"
+	"strconv"
+	"strings"
+	"testing"
+	"github.com/tolsen/slogger/v2"
+)
+
+func TestLog(test *testing.T) {
+	appender, logger := setup(test)
+
+	logger.Logf(slogger.WARN, "This is a log message")
+	logger.Flush()
+
+	assertCurrentLogContains(test, "This is a log message", appender)
+}
+
+func TestConcurrentLog(test *testing.T) {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	appender, logger := setup(test)
+
+	// Have 10 goroutines log 1000 lines each
+	for i := 0; i < 10; i++ {
+		prefix := fmt.Sprint("GO", i)
+		go logSomeLines(logger, prefix, 1000)
+	}
+
+	logger.Flush()
+
+	// Now check that each goroutine logged in order
+
+	stringAppender, ok := appender.Appender.(*slogger.StringAppender)
+	if !ok {
+		test.Fatal("sub Appender should be a *StringAppender: %v", appender.Appender)
+	}
+
+	tracker := make([]int, 10)
+	for i := 0; i < 10; i++ {
+		tracker[i] = -1
+	}
+	lineRegexp := regexp.MustCompile(`GO(\d+) (\d+)\n$`)
+	eof := false
+	for !eof {
+		line, err := stringAppender.ReadString('\n')
+		if err == io.EOF {
+			eof = true
+		} else if err != nil {
+			test.Fatal("Failure while reading log: " + err.Error())
+		}
+
+		matches := lineRegexp.FindAllStringSubmatch(line, 1)
+
+		if matches == nil {
+			continue
+		}
+
+		match := matches[0]
+		go_n, err := strconv.Atoi(match[1])
+		if err != nil {
+			test.Fatalf("Failure to parse %s as int: %s", match[1], err.Error())
+		}
+		seq, err := strconv.Atoi(match[2])
+		if err != nil {
+			test.Fatalf("Failure to parse %s as int: %s", match[2], err.Error())
+		}
+
+		if tracker[go_n] != seq - 1 {
+			test.Fatalf(
+				"Logged out of order?  Received seq %d for go %d when last seq was %d",
+				seq,
+				go_n,
+				tracker[go_n],
+			)
+		}
+
+		tracker[go_n] = seq
+	}
+
+	for i := 0; i < 10; i++ {
+		if tracker[i] != 999 {
+			test.Fatalf(
+				"Last received seq for go %d was %d",
+				i,
+				tracker[1],
+			)
+		}
+	}
+}	
+
+func assertCurrentLogContains(test *testing.T, expected string, appender *AsyncAppender) {
+	stringAppender, ok := appender.Appender.(*slogger.StringAppender)
+	if !ok {
+		test.Fatal("sub Appender should be a *StringAppender: %v", appender.Appender)
+	}
+	actual := stringAppender.String()
+
+	if !strings.Contains(actual, expected) {
+		test.Errorf("Log contains: \n%s\ninstead of\n%s", actual, expected)
+	}
+}
+
+func logSomeLines(logger *slogger.Logger, prefix string, numLines int) {
+	for i := 0; i < numLines; i++ {
+		logger.Logf(slogger.WARN, "%s %d", prefix, i)
+	}
+}
+
+func newAppenderAndLogger(test *testing.T) (appender *AsyncAppender, logger *slogger.Logger) {
+	appender = New(
+		slogger.NewStringAppender(new(bytes.Buffer)),
+		4096,
+		func(err error) {
+			msg := "Error during logging: " + err.Error()
+			fmt.Fprintln(os.Stderr, msg + "\n(Test may deadlock)")
+			test.Fatal(msg)
+		},
+	)
+
+	logger = &slogger.Logger{
+		Prefix: "rfa",
+		Appenders: []slogger.Appender{appender},
+	}
+	
+	return
+}
+	
+func setup(test *testing.T) (appender *AsyncAppender, logger *slogger.Logger) {
+	return newAppenderAndLogger(test)
+}
+
