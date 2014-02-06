efb9e530ec77921cd31b4185e481390b6be6c2b6
David Golub <david.golub@mongodb.com>
2014-02-06 11:32:12 -0500

    Allow FileAppender to work with any object that implements WriteString.

--- v1/appender.go
+++ v1/appender.go
@@ -22,8 +22,12 @@ func FormatLog(log *Log) string {
 		log.Message())
 }
 
+type StringWriter interface {
+	WriteString(s string) (ret int, err error)
+}
+
 type FileAppender struct {
-	*os.File
+	StringWriter
 }
 
 func (self FileAppender) Append(log *Log) error {
