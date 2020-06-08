5c0761a3a450e6b3f28a796d75f1b16f000d0f2b
TomerYakir <tomeryakir@gmail.com>
2020-06-08 15:11:49 +0300

    Synchronize context

--- v2/slogger/context.go
+++ v2/slogger/context.go
@@ -14,26 +14,36 @@
 
 package slogger
 
+import "sync"
+
 type Context struct {
 	fields map[string]interface{}
+	lock   sync.RWMutex
 }
 
 func NewContext() *Context {
 	c := new(Context)
 	c.fields = make(map[string]interface{}, 0)
+	c.lock = sync.RWMutex{}
 	return c
 }
 
 func (c *Context) Add(key string, value interface{}) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
 	c.fields[key] = value
 }
 
 func (c *Context) Get(key string) (value interface{}, found bool) {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
 	value, found = c.fields[key]
 	return
 }
 
 func (c *Context) Keys() []string {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
 	keys := make([]string, len(c.fields))
 	i := 0
 	for k, _ := range c.fields {
@@ -44,9 +54,13 @@ func (c *Context) Keys() []string {
 }
 
 func (c *Context) Len() int {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
 	return len(c.fields)
 }
 
 func (c *Context) Remove(key string) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
 	delete(c.fields, key)
 }
